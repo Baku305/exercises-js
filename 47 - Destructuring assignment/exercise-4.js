ae1360fd4156b1e892f361caaa662c558d5ce931
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-05-28 23:18:45 +0200

    fix47

--- 47 - Destructuring assignment/exercise-4.js
+++ 47 - Destructuring assignment/exercise-4.js
@@ -1,12 +1,25 @@
 const person = {
   id: 1,
-  firstName: 'Mario',
-  lastName: 'Rossi',
-  age: 25
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
 };
 
-let {id, firstName, lastName, age} = person
+let { id, firstName, lastName, age } = person;
 
-for (let item of Object.entries(person)) {
-  console.log(`${item[0]}: ${item[1]}`);
+for (let [key, value] of Object.entries(person)) {
+  console.log(`${key}: ${value}`);
 }
+
+// let {id, firstName, lastName, age} = person
+
+// const print = (id, firstName, lastName, age) => {
+//   this.id = id;
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.age = age;
+
+//   return this
+// }
+
+// console.log(print('1','mario','rossi',25));
