fb579f51318db19f6866907586c23c7fd4106e33
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-06-06 20:24:42 +0200

    fix41

--- 41 - error handling/exercise-1.js
+++ 41 - error handling/exercise-1.js
@@ -7,38 +7,57 @@ class BankAccount {
 
   deposit(amount) {
     // throw an exception if amount is negative
-    try {
+    try{
       if (amount > 0) {
         this.#amount += amount;
+        console.log(`${amount} added to account`);
       } else {
-        throw new Error('Amount must be positive'+ ', ' + `${amount}` + ' ' + 'is not a valid amount');
+        throw new Error(`Amount must be positive, ${amount} is not a valid amount`);
       }
-    }
+    } 
     catch (error) {
       console.log(error.message);
     }
   }
+  
 
   withdraw(amount) {
     // throw an exception if amount is negative or if withdrawal amount is greater than current amount
-    try {
-      if (amount > 0 && amount < 10000) {
-        this.#amount -= amount;
-      } else {
-        throw new Error(`Amount must be positive and less than 10000, ${amount} is not a valid amount`);
+      try {
+        if (amount > 0 && amount < 10000 && this.#amount >= amount) {
+          this.#amount -= amount;
+          console.log(`${amount} withdrawn from account`);
+        }
+        else if(amount >= 10000) {
+          throw new Error(`You can't withdraw more than 10000`);
+        }
+        else if (this.#amount < amount) {
+          throw new Error(`You don't have enough money in your account, can't withdraw more than ${this.#amount}`);
+        }
+        else {
+          throw new Error(`Amount must be positive, ${amount} is not a valid amount`);
+        }
       }
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
+        catch(error) {
+          console.log(error.message);
+        } 
+    } 
+
+
+  
 
   view() {
     console.log(this.#amount);
   }
 }
 
-const bankAccount = new BankAccount(1000);
-bankAccount.deposit(-500);
-bankAccount.deposit(200);
-bankAccount.withdraw(10000); // This operation should not be possible, because you cannot withdraw more than the account balance
-bankAccount.view();
+const bankAccount = new BankAccount(10000);
+setTimeout(() => bankAccount.deposit(100) , 1000);
+setTimeout(() => bankAccount.deposit(-500) , 2000);
+setTimeout(() => bankAccount.deposit(500) , 3000);
+setTimeout(() => bankAccount.withdraw(5000) , 4000);
+setTimeout(() => bankAccount.withdraw(10000) , 5000); // This operation should not be possible, because you cannot withdraw more than the account balance
+setTimeout(() => bankAccount.withdraw(7000) , 6000);
+setTimeout(() => bankAccount.withdraw(-10) , 8000);
+setTimeout(() => bankAccount.view() , 9000);
+
